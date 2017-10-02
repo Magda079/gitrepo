b1f4b1bdedda4a3eda5a38deab06bf1f209709a9
Magda079 <magdaa067@gmail.com>
2017-10-02 15:40:31 +0200

    Sumuj

--- cpp/sumuj.cpp
+++ cpp/sumuj.cpp
@@ -9,22 +9,16 @@
 using namespace std;
 
 int main(int argc, char **argv)
+
+
 {
-    int i;
-    int j;
-    
-    for (i = 1; i < 101; i++ )
-    {//umieszczamy blok kodu , który ma powtarzać
-        cout << "*" ; 
-        if (i % 10 == 0 ) 
-        {
-            for (j = 0 ; j < 9; j++)
-                cout << "#";
-            cout << endl; 
-        }
-    }
 
-    
-    return 0;
+    int suma =0;
+
+    for (int i=10; i<=20; i++) suma = suma +i;
+
+        cout << suma;
+
+return 0;
+
 }
-
