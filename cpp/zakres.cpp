005cc73a9f39a882035414a7a7b9847fe3001d90
Magda079 <magdaa067@gmail.com>
2017-10-17 09:54:42 +0200

    zakres

--- cpp/zakres.cpp
+++ cpp/zakres.cpp
@@ -8,21 +8,28 @@
 using namespace std; 
 
 //int liczba,krok; // zmienne globalne 
-int zwieksz() {
-    int liczba,krok; //zmienne lokalne
-    liczba = liczba + krok;
-    return liczba; 
+int zwieksz(int liczba, int krok) { //przekazywanie parametru przez wartość dostaje kopie
+    liczba = liczba + krok; // zmienne są lokalne dla tej funkcji
+    krok = 3* krok;
+    return krok; 
 }
- 
+
+void zwieksz2(int &a, int &b) { //& specjalny operator dzięki niemu dostaje dostęp do oryginałów
+    a = a + b;
+    b = 3*b;
+    
+}
+
 int main(int argc, char **argv)
 {
     int liczba,krok; // zmienne lokalne 
     cout << "Podaj liczbę i krok: " ;
     cin >> liczba >> krok;
     
-    cout << "Liczba i krok: " << liczba << " "<< krok << endl;
-    //zwieksz();
-    cout << "Liczba i krok: " << zwieksz() << " " << krok << endl;
+    cout << "Liczba: " << liczba << " "<< &liczba << endl;
+    
+    //zwieksz2(liczba, krok);
+    //cout << "Liczba i krok: " << liczba << " " << krok << endl;
     
 	return 0;
 }
