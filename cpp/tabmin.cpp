047c0b9c01d2fe40a9dd4f3ffbffba2ade0f1649
Magda079 <magdaa067@gmail.com>
2017-10-24 10:36:11 +0200

    tabela3

--- cpp/tabmin.cpp
+++ cpp/tabmin.cpp
@@ -23,15 +23,33 @@ void sumujliczby (int tab[], int ile){
          cout << "Suma liczb: " << suma << endl; 
 }
 
-void najmniejsza(int tab[], int min){
-    int i =0 ;
-    for (i = 0 ; i < min ; i++ ) {
+void najmniejsza(int tab[], int ile){
+    int min = tab[0];
+    int i = 0;
+    for (i = 1 ; i < ile ; i++ ) {
         if (min > tab[i])
         min = tab[i];
         } 
         cout << "Najmniejsza: " << min << endl;
         
     } // funkcja znajduje i drukuje najmniejsza liczbe z tabeli
+    
+void ile5(int tab[], int ile){
+    int i = 0;
+    for (i = 0 ; i < ile ; i++){
+        if (tab[i] %5  ==0)
+            licznik5++;
+            
+        if (tab[i] % 2 == 0 )
+            parzyste++;
+        
+        }
+        
+        
+    cout << "Liczby podzielne przez 5: " << licznik5 << endl;
+    cout << "Parzystych: " << parzyste << endl;
+    
+    }
 
 int main(int argc, char **argv)
 {
@@ -44,6 +62,7 @@ int main(int argc, char **argv)
     pobierzliczby(liczby, rozmiar);
     sumujliczby(liczby,rozmiar);
     najmniejsza(liczby,rozmiar);
+    ile5(liczby,rozmiar);
 
 	return 0;
 }
