8a04254f339ae483705f712445dfc23cf569b976
Magda079 <magdaa067@gmail.com>
2017-11-07 10:36:18 +0100

    dlotek2

--- python/dlotek.py
+++ python/dlotek.py
@@ -3,19 +3,21 @@
 
 import random
 
-del losuj(ileliczb, maksliczb):
-        liczby = [] #pusta lista
 
-    ile = 0 #ilość unikalnych liczb
-    #for i in range(ileliczb):
+def losuj(ileliczb, maksliczb):
+    liczby = []  # zbiór pusty
+
+    ile = 0
+    # for i in range(ileliczb):
     while ile < ileliczb:
         liczba = random.randint(0, maksliczb)
-        if liczby.count(liczba)== 0:
+        if liczby.count(liczba) == 0:
             liczby.append(liczba)
             ile += 1
 
     print(liczby)
     return liczby
+
 
 def main(args):
     ileliczb = int(input("Ile liczb chcesz zgadywać?"))
@@ -24,15 +26,18 @@ def main(args):
     liczby = losuj(ileliczb, maksliczb)
 
     # pobieranie typów użytkownika
-    typ = set() #pusty zbiór
-    for i in random(ileliczb):
+    typy = set()  # pusty zbiór
+    # for i in range(ileliczb):
+    ile = 0
+    while ile < ileliczb:
         typ = input("Podaj typ: ")
-        typy.add(typ)
+        if typ not in typy:
+            typy.add(typ)
+            ile += 1
 
     print(typy)
 
     return 0
-
 
 
 if __name__ == '__main__':
