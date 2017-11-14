431da2c3cfe40c17095938c4832b42f7be14bab6
Magda079 <magdaa067@gmail.com>
2017-11-14 10:35:51 +0100

    silnia

---
+++ cpp/silnia.cpp
@@ -0,0 +1,25 @@
+/*
+ * silnia.cpp
+ * 
+ */
+
+
+#include <iostream>
+using namespace std;
+
+int silnia_it();
+{
+    int wynik = 1
+    for (int i = 1 ; i < int n ; i++)
+        wynik = wynik * i 
+        cout << wynik << endl;
+}
+int main(int argc, char **argv)
+{
+	int n = 0
+    cout << "Podaj liczbę " << endl;
+    cin >> n
+    
+	return 0;
+}
+
