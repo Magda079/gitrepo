d4404f10fc810e7b528e3d84f5cdd129a4d11ac2
Magda079 <magdaa067@gmail.com>
2017-11-20 17:58:09 +0100

    nwd w cpp

--- python/silnia.py
+++ python/silnia.py
@@ -23,7 +23,7 @@ def main(args):
     assert silnia_it(1) == 1
     assert silnia_it(2) == 2
     assert silnia_it(3) == 6
-    #  print('Silnia dla {:d}: {:d}'.format(n,silnia_it))
+    print('Silnia dla {:d}: {:d}'.format(n,silnia_it))
     print (a ** b)
     return 0
 
