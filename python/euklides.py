2a34c035cefdaac13468e192090523f2b055f99c
Magda079 <magdaa067@gmail.com>
2017-11-21 10:35:26 +0100

    minmax

--- python/euklides.py
+++ python/euklides.py
@@ -7,18 +7,26 @@ def nwd_v1(a, b):
         if a > b:
             a = a - b
         else:
-            b = b - a
-    return a
+            b=b-a
+        return a 
+
+
+def nwd_v2(a, b):
+    while a > 0:
+        a = a%b
+        b = b - a
+    return b
 
 
 def main(args):
     a = int(input('Podaj liczbę naturalną'))
     b = int(input('Podaj liczbę naturalną '))
-    assert nwd_v1(5, 10) == 5
-    assert nwd_v1(3, 9) == 3
-    assert nwd_v1(4, 8) == 4
-    print("NWD({:d}, {:d}) = {:d}".format(a, b, nwd_v1(a, b)))
-    print('Największy wspolny dzielnik', nwd_v1(a, b))
+    assert nwd_v2(5, 10) == 5
+    assert nwd_v2(3, 9) == 3
+    assert nwd_v2(4, 8) == 4
+    print("NWD({:d}, {:d}) = {:d}".format(a, b, nwd_v2(a, b)))
+    print('Największy wspolny dzielnik', nwd_v2(a, b))
+    
     return 0
 
 
