e784a33d916f1a958ec7ad95e7a871d070d48735
Magda079 <magdaa067@gmail.com>
2017-12-11 12:31:06 +0100

    potega_rek

--- python/potega.py
+++ python/potega.py
@@ -2,7 +2,8 @@
 # -*- coding: utf-8 -*-
 #
 #  potega.py
-#  
+#
+
 
 def potega(a,b):
     wynik = 1
@@ -11,22 +12,23 @@ def potega(a,b):
     return wynik
 
 
+#  potega_rek (a, 0) = 1 dla a różnego od 0
+#  potega_rek(a, n) = potega_rek(n - 1) * a dla n = N+
+
+def potega_rek(a, n):
+    if n == 0:
+        return 1
+    return potega_rek(n - 1) * a
+
+
 def main(args):
-    a=int(input('Podaj podstawę '))
-    b=int(input('Podaj wykładnik '))
-    
-    assert type (a) == int
-    assert type (b) == int 
-    
-    #while b < 0:
-        #wynik = a ** b 
-   
-    assert potega (0) == 0
-    assert potega (1) == 1
-    assert potega (2) == 4
-    assert potega (3) == 9
-        
-    print ( a ** b )
+    a = int(input('Podaj podstawę '))
+    b = int(input('Podaj wykładnik '))
+
+#  while b < 0:
+#  wynik = a ** b
+
+    print (a ** b)
     return 0
 
 if __name__ == '__main__':
