9df82503bd413f7c425c4833e60609e7eaa3cf52
Magda079 <magdaa067@gmail.com>
2017-12-18 12:35:02 +0100

    sortuj

--- cpp/sort_wybor.cpp
+++ cpp/sort_wybor.cpp
@@ -25,12 +25,34 @@ void drukuj(int t[], int n){
     
 }
 
+void zamien(int &a, int &b){ // & operator odczytywania adresu
+        int tmp = a;
+        a = b;
+        b = tmp;
+}
+
+void sort_wyb(int t[], int n){
+        //selection sort
+        cout << "------Sortowanie przez wybieranie------"<< endl;
+        int i, j, k;
+        for (i = 0; i < n; i++){
+                k = i;
+                for (j = i + 1; j < n; j++){
+                    if (t[j] < t[k])
+                        k = j;
+                }
+                zamien(t[i], t[k]);
+        }
+}
+
 int main(int argc, char **argv)
 {
 	const int ile = 10;
     int tab[ile];
     wypelnij(tab,ile,20);
     drukuj(tab,ile);
+    sort_wyb(tab, ile);
+    drukuj(tab, ile);
 	return 0;
 }
 
