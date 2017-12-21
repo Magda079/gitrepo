7d85d1df76de333f15f157f94b1ccddc5bd61cfe
Magda079 <magdaa067@gmail.com>
2017-12-21 20:22:45 +0100

    sortowanie18.12

--- python/sort_wybor_Lasota.py
+++ python/sort_wybor_Lasota.py
@@ -5,42 +5,40 @@
 #  
 #  
 
-import random
+from random import randint
 
 
-def wypelnij(liczby, ileliczb, maks):
-
-    ile = 0  
-    
-    while ile < ileliczb:
-        liczba = random.randint(0, maks)
-        if liczby.count(liczba) == 0:
-            liczby.append(liczba)
-            ile +=1
-
-    return liczby
-
-
-def sort_wybor(tab):
-    
-    print("------- Sortowanie przez wybieranie ------")
-    for i in range(len(tab)):
-        k = i
-        for j in range(i + 1, len(tab)):
-            if tab[j] < tab[k]:
-                k = j
-        tab[i], tab[k] = tab[k], tab[i]
+def wypelnij(tab, n, m):
+    ile = 0
+    while ile < n:
+        liczba = randint(0, m)
+        if tab.count(liczba) == 0:
+            tab.append(liczba)
+            ile += 1
     return tab
+
+
+def sort_wyb(tab, n):
+    for i in range(0, n):
+        k = i
+        j = i + 1
+        for j in range(j, n + 1):
+            if tab[j] < tab[k]:
+                tab[j], tab[k] = tab[k], tab[j]
+    return tab
+
+
 
 
 def main(args):
     ile = 10
-    tab = []
-    maks = int(input('Podaj maksymalną liczbe: '))
-    print(wypelnij(tab, ile, maks))
-    print(sort_wybor(tab))
-    return 0
+    tab = [ile]
+    max = int(input("Jaka maksymalna liczbe chcesz wlozyc do tabeli: "))
+    print("Tabela przed sortowaniem: ", wypelnij(tab, ile, max))
+    print("Tabela po sortowaniu: ", sort_wyb(tab, ile))
 
+    
+    return 0
 
 if __name__ == '__main__':
     import sys
