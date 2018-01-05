b8038de5fa4c62e2f81ca997e2c865864653f6d5
Magda079 <magdaa067@gmail.com>
2018-01-05 08:44:59 +0100

    binarne

--- python/horner.py
+++ python/horner.py
@@ -2,26 +2,25 @@
 # -*- coding: utf-8 -*-
 #
 #  horner.py
-#  
+#
 
 
 def horner_it(stopien, tbwsp, x):
 	wynik = tbwsp[0]
 	for i in range(1, stopien + 1):
 		wynik = wynik * x + tbwsp[i]
-	
-	return wynik\
-    
+
+	return wynik
 
 
 def main(args):
 	tbwsp = []
 	stopien = 3
 	x = int(input("Podaj wartośc x: "))
-	for i in range(0, 4):		
+	for i in range(0, 4):
 		tmp = int(input("Podaj współczynniki: "))
 		tbwsp.append(tmp)
-		
+
 	print("Wynik: ", horner_it(stopien, tbwsp, x))
 	return 0
 
