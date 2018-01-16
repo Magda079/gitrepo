fffafce8d03a874d2c97c23e872d0f2e4c41665f
Magda079 <magdaa067@gmail.com>
2018-01-16 10:35:46 +0100

    algorytmy

---
+++ cpp/zlozonosc_alg1.cpp
@@ -0,0 +1,25 @@
+/*
+ * zlozonosc_alg1.cpp
+ * 
+ * Copyright 2018  <>
+ * 
+ */
+
+
+#include <iostream>
+using namespace std; 
+
+int main(int argc, char **argv)
+{
+	int a = 0;
+    cout << "Podaj a: "<< endl;
+    cin >> a;
+    
+   
+        
+        
+        
+
+	return 0;
+}
+
