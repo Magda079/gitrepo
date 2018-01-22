71d6d6117a976c6c29dd4a2818ecf4323b3a658d
Magda079 <magdaa067@gmail.com>
2018-01-22 12:32:39 +0100

    zlozonosc2

---
+++ cpp/Lasota_zlozonosc_alg1.cpp
@@ -0,0 +1,31 @@
+/*
+ * zlozonosc_alg1.cpp
+ * 
+ * Copyright 2018  <>
+ * 
+ */
+
+
+#include <iostream>
+using namespace std; 
+
+int main(int argc, char **argv)
+{
+	int a = 0;
+    cout << "Podaj a: "<< endl;
+    cin >> a;
+    
+     for (int i=1;i < a;i += 2){
+       
+        if (a > 0 && a <100 ){
+            cout<< a << endl;
+        }
+            
+    
+    
+
+    
+    
+	return 0;
+}
+
