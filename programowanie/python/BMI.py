f40275dc7364c5dc7b4ae572847c3a4ac89050c0
magda079 <magdaa067@gmail.com>
2018-09-10 09:39:14 +0200

    yx

---
+++ programowanie/python/BMI.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  BMI.py
+#  
+
+
+def main(args):
+
+    masa = input('Podaj masę:')
+    wzrost = input('Podaj wzrost:') 
+    
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
