5ecc112b39bc6cc32240f0b89c033b9b575c6008
magda079 <magdaa067@gmail.com>
2018-10-26 13:25:58 +0200

    baza

--- bazy/gus/dane_uczniow/kwerendy.py
+++ bazy/gus/dane_uczniow/kwerendy.py
@@ -10,23 +10,39 @@ import sqlite3
 
 def kwerenda1(cur):
     cur.execute("""
-        SELECT nazwisko, imie1, dzien, miesiac, rok FROM nazwiska
-        INNER JOIN dane_osobowe 
-        ON nazwiska.nr_ucznia=dane_osobowe.nr_ucznia
-        WHERE nazwiska.nr_ucznia=9201
+        SELECT klasa, nazwisko, imie FROM klasy
+        INNER JOIN uczniowie
+        ON klasy.id=uczniowie.id_klasa
     """)
+    
     #  SELECT * FROM nazwiska WHERE nazwisko LIKE 'G%'
     wyniki = cur.fetchall() 
     for row in wyniki:
         print(tuple(row))
 
+         #WHERE nazwiska.nr_ucznia=
+        #(SELECT nr_ucznia FROM nazwiska
+        #WHERE nazwisko='German' AND imie1='Dariusz') #oceny
 
+        #SELECT nazwisko, imie1, dzien, miesiac, rok FROM nazwiska
+        #INNER JOIN dane_osobowe 
+        #ON nazwiska.nr_ucznia=dane_osobowe.nr_ucznia
+        #WHERE nazwiska.nr_ucznia=9201
+        
+        #SELECT nazwisko, imie1, dzien, miesiac, rok FROM nazwiska
+        #INNER JOIN dane_osobowe 
+        #ON nazwiska.nr_ucznia=dane_osobowe.nr_ucznia
+        #WHERE nazwiska.nr_ucznia=
+        #(SELECT nr_ucznia FROM nazwiska
+        #WHERE nazwisko='Gryczon' AND imie1='Agata')
 
 
 def main(args):
     ### KONFIGURACJA ###
-    baza_nazwa = 'szkola'
-    tabele = ['nazwiska', 'dane_osobowe', 'oceny']
+    baza_nazwa = 'uczniowie'
+    tabele = ['uczniowie', 'klasy', 'przedmioty', 'oceny']
+    roz = '.csv'
+    naglowki = False
     ####################
     
     con = sqlite3.connect(baza_nazwa + '.db')
