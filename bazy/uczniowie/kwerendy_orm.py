3765a6449dc98ebbc3f768bdba26e326bf836470
magda079 <magdaa067@gmail.com>
2018-11-30 13:04:16 +0100

    orm_kwerendy

--- bazy/uczniowie/kwerendy_orm.py
+++ bazy/uczniowie/kwerendy_orm.py
@@ -6,14 +6,29 @@
 from uczniowie_model import * 
 
 def kw01(): 
-    query = Uczen.select().where(Uczen.imie.startswith('G')) # dolowna zmienna , ta wybiera wszystkich uczniów z klasy 
+    # query = Uczen.select().where(Uczen.imie.startswith('G')) # dolowna zmienna , ta wybiera wszystkich uczniów z klasy 
+    # query = Uczen.select().where(Uczen.imie == 'Rafał')
+    # query = Uczen.select().where(Uczen.imie == 'Rafał').count()
+    # print(query)
+    query = (Uczen
+        .select()
+        .where(Uczen.egz_mat.between(30,35))
+        .order_by(Uczen.egz_mat.asc())) # asc - rosnąco | desc - malejąco 
     for obj in query:
-        print(obj.nazwisko, obj.imie, obj.egz_hum)
+        print(obj.nazwisko, obj.imie, obj.egz_mat)
 
+def kw02():
+    query = (Uczen
+        .select(Uczen.nazwisko, Uczen.klasa.klasa)
+        .join(Klasa)
+        .order_by(Uczen.klasa.klasa))
+    for obj in query:
+        print(obj.nazwisko, obj.klasa.klasa)
+    
 def main(args):
     baza.connect()
     
-    kw01()
+    kw02()
     
     baza.close()
     return 0
