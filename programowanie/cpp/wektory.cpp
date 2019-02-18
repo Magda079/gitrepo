c1b0b3b48fbd90353210009b7a55c8eb43bc9f43
magda079 <magdaa067@gmail.com>
2019-02-18 09:18:35 +0100

    xx

--- programowanie/cpp/wektory.cpp
+++ programowanie/cpp/wektory.cpp
@@ -10,10 +10,11 @@
 
 using namespace std;
 
+#define ILE 2
+
 struct punkt{
     int x;
     int y;
-        
 }
 
 struct wektor{
@@ -21,9 +22,18 @@ struct wektor{
     punkt pk; 
 }
 
+void drukuj(punkt []){
+    for(int i=0; i < ILE; i++){
+        cout << "Punkt: " << endl;
+        cout << setw(10) << left << "X: "; cout << tb[i].x << endl; 
+        cout << setw(10) << left << "Y: "; cout << tb[i].y << endl;
+        }    
+}
+
 int main(int argc, char **argv)
 {
-	
+
+        
 	return 0;
 }
 
