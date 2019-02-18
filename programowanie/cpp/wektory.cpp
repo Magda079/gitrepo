b36adb2005b75183d978cf6ec0dd5d46319402d3
magda079 <magdaa067@gmail.com>
2019-02-18 09:12:07 +0100

    xx

---
+++ programowanie/cpp/wektory.cpp
@@ -0,0 +1,29 @@
+/*
+ * wektory.cpp 
+ * 
+ */
+
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+
+using namespace std;
+
+struct punkt{
+    int x;
+    int y;
+        
+}
+
+struct wektor{
+    punkt pp;
+    punkt pk; 
+}
+
+int main(int argc, char **argv)
+{
+	
+	return 0;
+}
+
