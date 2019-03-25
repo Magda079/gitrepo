ebe1ac4cc694d2a061958a48b81d28a35004fb21
magda079 <magdaa067@gmail.com>
2019-03-25 09:29:52 +0100

    xxx

--- programowanie/cpp/lista.cpp
+++ programowanie/cpp/lista.cpp
@@ -11,6 +11,40 @@ struct node {
   node * nast;  
 };
 
+char czy() {
+    char odp;
+    cout << "Natępny element (t/n)? ";
+    cin >> odp;
+    return odp;
+}
+
+int getD() {
+    int d;
+    cout << "Podaj liczbę: ";
+    cin >> d;
+    return d;    
+    
+}
+
+void drukuj(node *glowa) {
+    node *tmp;
+    tmp = glowa;
+    while(tmp != NULL) {
+        cout << tmp->liczba << " ";
+        tmp = tmp->nast;
+    } 
+    cout << endl;
+}
+
+void delLista(node *glowa) {
+    node *tmp;
+    while(glowa != NULL) {
+        tmp = glowa;
+        glowa = glowa->nast;
+        delete tmp;
+    }
+}
+
 int addToB() {
     node *glowa = NULL;
     node *nowy;
@@ -26,6 +60,11 @@ int addToB() {
         nowy ->liczba = getD();
         glowa = nowy;
     }
+    
+    cout << "Zawartość listy : " << endl;
+    drukuj(glowa);
+    delLista(glowa);
+    return 0;
 }
 
 int main(int argc, char **argv)
